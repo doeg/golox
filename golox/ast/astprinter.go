78c93fb7497e370da5a659f38091f12c900331f9
doeg <855595+doeg@users.noreply.github.com>
2022-12-30 16:12:28 -0500

    Refactor AST to have public methods, update all visitor return types to (any, error)

--- golox/ast/astprinter.go
+++ golox/ast/astprinter.go
@@ -8,35 +8,40 @@ import (
 type ASTPrinter struct{}
 
 func (p *ASTPrinter) Print(expr Expr) string {
-	result := expr.accept(p).(string)
-	return result
+	result, _ := expr.Accept(p)
+	return result.(string)
 }
 
-func (p *ASTPrinter) parenthesize(name string, exprs ...Expr) string {
+func (p *ASTPrinter) parenthesize(name string, exprs ...Expr) (string, error) {
 	strs := make([]string, 0)
 	for _, expr := range exprs {
-		strs = append(strs, expr.accept(p).(string))
+		s, err := expr.Accept(p)
+		if err != nil {
+			return "", err
+		}
+
+		strs = append(strs, s.(string))
 	}
 
-	return fmt.Sprintf("(%s %s)", name, strings.Join(strs, " "))
+	return fmt.Sprintf("(%s %s)", name, strings.Join(strs, " ")), nil
 }
 
-func (p *ASTPrinter) visitBinaryExpr(expr *Binary) any {
+func (p *ASTPrinter) VisitBinaryExpr(expr *Binary) (any, error) {
 	return p.parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right)
 }
 
-func (p *ASTPrinter) visitGroupingExpr(expr *Grouping) any {
+func (p *ASTPrinter) VisitGroupingExpr(expr *Grouping) (any, error) {
 	return p.parenthesize("group", expr.Expression)
 }
 
-func (p *ASTPrinter) visitLiteralExpr(expr *Literal) any {
+func (p *ASTPrinter) VisitLiteralExpr(expr *Literal) (any, error) {
 	if expr.Value == nil {
-		return "nil"
+		return nil, nil
 	}
 
-	return fmt.Sprintf("%+v", expr.Value)
+	return fmt.Sprintf("%+v", expr.Value), nil
 }
 
-func (p *ASTPrinter) visitUnaryExpr(expr *Unary) any {
+func (p *ASTPrinter) VisitUnaryExpr(expr *Unary) (any, error) {
 	return p.parenthesize(expr.Operator.Lexeme, expr.Right)
 }
