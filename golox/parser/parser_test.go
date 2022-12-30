13f5d8257f9e0254bfae38d7c5aaba9a036fdec5
doeg <855595+doeg@users.noreply.github.com>
2022-12-30 13:33:06 -0500

    Add a first pass at parser.go + expression parsing (#7)

---
+++ golox/parser/parser_test.go
@@ -0,0 +1,229 @@
+package parser
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/doeg/golox/golox/ast"
+	"github.com/doeg/golox/golox/scanner"
+	"github.com/doeg/golox/golox/token"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		testName      string
+		input         string
+		expected      ast.Expr
+		expectedError error
+	}{
+		{
+			input:    "123",
+			expected: &ast.Literal{Value: float64(123)},
+		},
+		{
+			input:    "\"hello, world\"",
+			expected: &ast.Literal{Value: "hello, world"},
+		},
+		{
+			input:    "true",
+			expected: &ast.Literal{Value: true},
+		},
+		{
+			input:    "false",
+			expected: &ast.Literal{Value: false},
+		},
+		{
+			input:    "nil",
+			expected: &ast.Literal{Value: nil},
+		},
+		{
+			input: "-1",
+			expected: &ast.Unary{
+				Operator: &token.Token{
+					Lexeme: "-",
+					Line:   0,
+					Type:   token.MINUS,
+				},
+				Right: &ast.Literal{Value: float64(1)},
+			},
+		},
+		{
+			input: "5 - 3 - 1",
+			expected: &ast.Binary{
+				Left: &ast.Binary{
+					Left: &ast.Literal{
+						Value: float64(5),
+					},
+					Operator: &token.Token{
+						Lexeme: "-",
+						Line:   0,
+						Type:   token.MINUS,
+					},
+					Right: &ast.Literal{
+						Value: float64(3),
+					},
+				},
+				Operator: &token.Token{
+					Lexeme: "-",
+					Line:   0,
+					Type:   token.MINUS,
+				},
+				Right: &ast.Literal{
+					Value: float64(1),
+				},
+			},
+		},
+		{
+			input: "5 - 3 * 1",
+			expected: &ast.Binary{
+				Left: &ast.Literal{
+					Value: float64(5),
+				},
+				Operator: &token.Token{
+					Lexeme: "-",
+					Line:   0,
+					Type:   token.MINUS,
+				},
+				Right: &ast.Binary{
+					Left: &ast.Literal{
+						Value: float64(3),
+					},
+					Operator: &token.Token{
+						Lexeme: "*",
+						Line:   0,
+						Type:   token.STAR,
+					},
+					Right: &ast.Literal{
+						Value: float64(1),
+					},
+				},
+			},
+		},
+		{
+			input: "(5 - 3) * 1",
+			expected: &ast.Binary{
+				Left: &ast.Grouping{
+					Expression: &ast.Binary{
+						Left: &ast.Literal{
+							Value: float64(5),
+						},
+						Operator: &token.Token{
+							Lexeme: "-",
+							Line:   0,
+							Type:   token.MINUS,
+						},
+						Right: &ast.Literal{
+							Value: float64(3),
+						},
+					},
+				},
+				Operator: &token.Token{
+					Lexeme: "*",
+					Line:   0,
+					Type:   token.STAR,
+				},
+				Right: &ast.Literal{
+					Value: float64(1),
+				},
+			},
+		},
+		{
+			input: "-69 < 420 == true",
+			expected: &ast.Binary{
+				Left: &ast.Binary{
+					Left: &ast.Unary{
+						Operator: &token.Token{
+							Lexeme: "-",
+							Line:   0,
+							Type:   token.MINUS,
+						},
+						Right: &ast.Literal{Value: float64(69)},
+					},
+					Operator: &token.Token{
+						Lexeme: "<",
+						Line:   0,
+						Type:   token.LESS,
+					},
+					Right: &ast.Literal{
+						Value: float64(420),
+					},
+				},
+				Operator: &token.Token{
+					Lexeme: "==",
+					Line:   0,
+					Type:   token.EQUAL_EQUAL,
+				},
+				Right: &ast.Literal{Value: true},
+			},
+		},
+		{
+			testName:      "error: expected expression",
+			input:         "* 1",
+			expectedError: errors.New(ErrExpectExpression),
+		},
+		{
+			testName:      "error: missing closing paren",
+			input:         "(1",
+			expectedError: errors.New(ErrExpectClosingParen),
+		},
+	}
+
+	for _, tt := range tests {
+		testName := tt.testName
+		if tt.testName == "" {
+			testName = tt.input
+		}
+
+		tt := tt
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			s := scanner.New([]byte(tt.input))
+			tokens, errors := s.ScanTokens()
+			require.Empty(t, errors)
+
+			p := New(tokens)
+			expr, err := p.Parse()
+
+			if tt.expectedError != nil {
+				assert.Nil(t, expr)
+				assert.Equal(t, tt.expectedError, err)
+			} else {
+				assert.EqualValues(t, tt.expected, expr)
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestSynchronize(t *testing.T) {
+	tests := []struct {
+		input         string
+		expectedIndex int
+	}{
+		{
+			input:         "(1 + 2; 3;",
+			expectedIndex: 5,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.input, func(t *testing.T) {
+			t.Parallel()
+
+			s := scanner.New([]byte(tt.input))
+			tokens, errors := s.ScanTokens()
+			require.Empty(t, errors)
+
+			p := New(tokens)
+			err := p.synchronize()
+			require.Nil(t, err)
+
+			assert.Equal(t, tt.expectedIndex, p.current)
+		})
+	}
+}
