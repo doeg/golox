3b83413fa7ee15da888e673812a9bbe6e01fbd38
doeg <855595+doeg@users.noreply.github.com>
2023-01-01 16:37:30 -0500

    Add a repl entrypoint to cmd/golox

--- cmd/golox/main.go
+++ cmd/golox/main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/doeg/golox/golox/interpreter"
+	"github.com/doeg/golox/golox/parser"
+	"github.com/doeg/golox/golox/scanner"
+)
 
 func main() {
-	greeting := "Hello world!"
-	fmt.Println(greeting)
+	repl()
+}
+
+func repl() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+
+		s := scanner.New([]byte(text))
+		tokens, errs := s.ScanTokens()
+		if len(errs) > 0 {
+			for _, err := range errs {
+				fmt.Println(err.Error())
+			}
+			continue
+		}
+
+		p := parser.New(tokens)
+		expr, err := p.Parse()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		i := interpreter.New()
+		result, err := i.Interpret(expr)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Printf("%+v\n", result)
+	}
 }
