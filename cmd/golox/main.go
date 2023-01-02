de0b25595e8c7674873f4a54da2ba063c3127f0b
Sara Bee <855595+doeg@users.noreply.github.com>
2023-01-02 15:29:15 -0500

    Update golox cmd to read from file

--- cmd/golox/main.go
+++ cmd/golox/main.go
@@ -11,7 +11,50 @@ import (
 )
 
 func main() {
-	repl()
+	switch len(os.Args) {
+	case 1:
+		repl()
+	case 2:
+		fromFile(os.Args[1])
+	default:
+		fmt.Println("Usage: golox [filename]")
+	}
+}
+
+func execute(input []byte) error {
+	s := scanner.New([]byte(input))
+	tokens, errs := s.ScanTokens()
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Println(err.Error())
+		}
+		return nil
+	}
+
+	p := parser.New(tokens)
+	stmts, err := p.Parse()
+	if err != nil {
+		return err
+	}
+
+	i := interpreter.New()
+
+	// TODO consider automatically printing the value of evaluated expressions...?
+	_, err = i.Interpret(stmts)
+	return err
+}
+
+func fromFile(filename string) {
+	fmt.Println(filename)
+
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = execute(input); err != nil {
+		panic(err)
+	}
 }
 
 func repl() {
@@ -20,28 +63,8 @@ func repl() {
 		fmt.Print("> ")
 		text, _ := reader.ReadString('\n')
 
-		s := scanner.New([]byte(text))
-		tokens, errs := s.ScanTokens()
-		if len(errs) > 0 {
-			for _, err := range errs {
-				fmt.Println(err.Error())
-			}
-			continue
-		}
-
-		p := parser.New(tokens)
-		stmts, err := p.Parse()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		i := interpreter.New()
-
-		// TODO consider automatically printing the value of evaluated expressions...?
-		_, err = i.Interpret(stmts)
-		if err != nil {
-			fmt.Println(err)
+		if err := execute([]byte(text)); err != nil {
+			fmt.Println("error: ", err)
 			continue
 		}
 	}
